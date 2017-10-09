06ff743b6dc61e6c72034e782cc6cd4793174fea
Pramod S Vidyarthi <pramod@corp.optym.net>
2017-10-09 15:23:37 +0530

    route masking using 'as' prop

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ const layoutStyle = {
 
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -19,9 +19,9 @@ export default props => (
   <div style={layoutStyle}>
     <Header />
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next.js is awesome" />
-      <PostLink title="Deploy apps with Zeit" />
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
     </ul>
     {props.children}
   </div>
