3acd3c68cd57b02d26726d979c3637baa57afb81
Pramod <pramodsvidyarthi@gmail.com>
2017-10-09 19:20:36 +0530

    Fetching data for pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
-import Link from "next/link";
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
-export default props => (
+export const Greet = props => (
   <Layout>
     <h1>Hello Next.js</h1>
   </Layout>
 );
+
+export default class Index extends React.Component {
+  static getInitialProps = async() => {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+ 
+    return { shows: data };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Batman TV shows</h1>
+        <ul>
+          {this.props.shows.map(({ show }) => (
+            <li key={show.id}>
+              <Link as={`/p/${show.id}`} href={`/post?id={show.id}`}>
+                <a>{show.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
