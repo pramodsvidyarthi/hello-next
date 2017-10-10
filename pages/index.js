757fa98243573cb2b425a51253570601bbde194d
Pramod <pramodsvidyarthi@gmail.com>
2017-10-10 07:56:45 +0530

    css in js using styled jsx

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
-import Layout from '../components/Layout';
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+import Layout from "../components/Layout";
 
 export const Greet = props => (
   <Layout>
@@ -9,13 +9,14 @@ export const Greet = props => (
 );
 
 export default class Index extends React.Component {
-  static getInitialProps = async() => {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  static getInitialProps = async () => {
+    const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
     const data = await res.json();
- 
-    return { shows: data };
-  }
 
+    return { shows: data };
+  };
+// styled jsx has no effect for elements inside of child.
+// or use global prop in style component.
   render() {
     return (
       <div>
@@ -29,6 +30,30 @@ export default class Index extends React.Component {
             </li>
           ))}
         </ul>
+        <style jsx>{`
+          h1,
+          a {
+            font-family: "Arial";
+          }
+
+          ul {
+            padding: 0;
+          }
+
+          li {
+            list-style: none;
+            margin: 5px 0;
+          }
+
+          a {
+            text-decoration: none;
+            color: blue;
+          }
+
+          a:hover {
+            opacity: 0.6;
+          }
+        `}</style>
       </div>
     );
   }
